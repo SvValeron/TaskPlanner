83e217874acf928e286ad85045ad57e994dec55a
Валерій Святецький <ahma020293@gmail.com>
2024-02-07 16:22:14 +0200

    Add Startup and change project

--- TaskPlannerAPI/Program.cs
+++ TaskPlannerAPI/Program.cs
@@ -4,33 +4,18 @@ namespace TaskPlanner;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        CreateWebHostBuilder(args).Build().Run();
+    }
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddDbContext<TaskPlannerContext>();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+    public static IHostBuilder CreateWebHostBuilder(string[] args)
+    {
+         return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        
     }
 }
---
+++ TaskPlannerAPI/Startup.cs
@@ -0,0 +1,31 @@
+﻿using TaskPlanner.DB;
+
+namespace TaskPlanner;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddDbContext<TaskPlannerContext>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseRouting();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+    }
+}
