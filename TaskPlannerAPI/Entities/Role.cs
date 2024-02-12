b65e320e30a6e52d938eb0408dcaed04a8eee0fb
Валерій Святецький <ahma020293@gmail.com>
2024-02-12 14:10:16 +0200

    Try use Identity, change class Role

---
+++ TaskPlannerAPI/Entities/Role.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskPlanner.Entities;
+
+[Table("roles")]
+
+public class Role// : IdentityRole<int>
+{
+    [Key]
+    [Column("Id")]
+    public int Id { get; set; }
+
+    [Column("Name")]
+    [Required]
+    public string Name { get; set; } = default!;
+
+}
